eee1cbdd319dc7e1bea1991830931d0d2b0aa9f3
ancajeckel <45466406+ancajeckel@users.noreply.github.com>
2019-03-06 16:58:10 +0200

    Problem 7

---
+++ GenericsApp/Problem07.CentsRepresentation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem07.CentsRepresentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter amount:");
+            int amount = Convert.ToInt32(Console.ReadLine());
+
+            if (amount == 0)
+            {
+                Console.WriteLine("Amount 0, no combinations available");
+                return;
+            }
+
+            //quarters (25 cents), dimes (10 cents), nickels (5 cents) and pennies (1 cent)
+
+            for (int i = 0; i <= amount / 25; i++)
+            {
+                for (int j = 0; j <= amount / 10; j++)
+                {
+                    for (int k = 0; k <= amount / 5; k++)
+                    {
+                        for (int m = 0; m <= amount / 1; m++)
+                        {
+                            var current = i * 25 + j * 10 + k * 5 + m * 1;
+                            if (current == amount)
+                            {
+                                Console.Write($"({i} {j} {k} {m}) ");
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
